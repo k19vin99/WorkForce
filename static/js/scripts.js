c421c0d4088ec3639d7e3f06c86e551d47b9b4bb
k19vin99 <KevinCespedes.ExtLogicalis@consorcio.cl>
2024-10-22 18:05:22 -0300

    Eliminación de css en desuso

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,3 +6,12 @@ document.addEventListener("DOMContentLoaded", function() {
 document.addEventListener('DOMContentLoaded', function () {
   AOS.init();
 });
+
+/* global bootstrap: false */
+(() => {
+  'use strict'
+  const tooltipTriggerList = Array.from(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  tooltipTriggerList.forEach(tooltipTriggerEl => {
+    new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})()
